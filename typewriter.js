873b2698b7e842e1123ec2ee870387b7df3c2164
Victor Ly <ly.victor2000@gmail.com>
2022-08-30 13:46:12 +0200

    first commit

---
+++ typewriter.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let typewrittenText;
+
+document.addEventListener("DOMContentLoaded", init);
+
+//initialize
+function init() {
+  typewrittenText = document.querySelectorAll(".typewritten");
+}
